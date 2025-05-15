931c3d358b36912d9c55163b697653c98ec69252
Valentin <valentin.grimm@th-owl.de>
2025-05-15 11:29:52 +0200

    update model

--- news_classification.py
+++ news_classification.py
@@ -2,7 +2,7 @@ from llama_model import LlamaModel
 from ResponseModels.ClassificationResponse import ClassificationResponse
 from typing import Type
 
-class NewsClassification(LlamaModel[ClassificationResponse]):
+class NewsClassification(LlamaModel[ClassificationResponse]("llama3:70b")):
     
     def get_response_model(self) -> Type[ClassificationResponse]:
         return ClassificationResponse
